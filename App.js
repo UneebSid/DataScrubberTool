8baad86365cb209a9b4f96994e3519ffcf6cad25
UneebSidd <90067237+UneebSidd@users.noreply.github.com>
2023-08-01 14:37:56 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
-import {Container, Button, Table, TableBody,TableCell,TableContainer, TableHead, TableRow, Paper, Chip, Divider, Box, TextField, Dialog, DialogTitle, DialogContent, DialogActions} from "@mui/material";
+import * as React from 'react'
+import {Container, Button, Table, TableBody,TableCell,tableCellClasses,TableContainer, TableHead, TableRow, Paper, Chip, TextField, Dialog, DialogTitle, DialogContent, DialogActions, TableFooter, Pagination, IconButton, TablePagination,} from "@mui/material";
 import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddIcon from '@mui/icons-material/Add';
-import axios from 'axios';
+import { styled } from '@mui/material/styles';
+
 import './App.css';
 
 function App() {
- 
+
 const [data, setData] = useState([])
 const [showForm, setShowForm] = useState(false);
 const [newRowData, setNewRowData] = useState({
@@ -19,6 +19,29 @@ const [newRowData, setNewRowData] = useState({
   state: '',
   city: '',
 });
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+  
+
+
 
 
 useEffect(()=> {
@@ -36,6 +59,7 @@ const handleAddRow = () => {
 
 const handleCloseForm = () => {
   setShowForm(false);
+  
   setNewRowData({
     id: '',
     firstname: '',
@@ -74,57 +98,61 @@ const handleAddNewRow = () => {
 const  onDelete = (index)=> {
 
         const list=[...data]
-        list.splice(index,1)
-        setData(list);
-
         fetch("http://localhost:5000/deletePatient", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body:JSON.stringify(index)
+        body:JSON.stringify(list[index].id)
       }).catch((error)=>{
         console.error('Error: ', error)
       })
+      list.splice(index,1)
+        setData(list);
 
 }
 
 
   return (
+    
     <div className="App">
     
           {
        <Container>
         <h1>Data Scrubber Tool</h1>
-          <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
+          <TableContainer component={Paper} sx={{
+            color:'success.main'
+            }}>
+          <Table sx={{ minWidth: 650}} size="small" stickyHeader aria-label="simple table">
+            
+             <TableHead>
              <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell align="right">First Name</TableCell>
-            <TableCell align="right">Last Name</TableCell>
-            <TableCell align="right">Age</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">State</TableCell>
-            <TableCell align="right">City</TableCell>
+            <StyledTableCell>Id</StyledTableCell>
+            <StyledTableCell align="right">First Name</StyledTableCell>
+            <StyledTableCell align="right">Last Name</StyledTableCell>
+            <StyledTableCell align="right">Age</StyledTableCell>
+            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="right">State</StyledTableCell>
+            <StyledTableCell align="right">City</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row, index) => (
-            row ? (<TableRow
-              key={index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.firstname}</TableCell>
-              <TableCell align="right">{row.lastname}</TableCell>
-              <TableCell align="right">{row.age}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.state}</TableCell>
-              <TableCell align="right">{row.city}</TableCell>
-              <TableCell align="right">
+          {
+          data
+          .map((row, index) => (
+            row ? (<StyledTableRow
+                    key={index}
+                              >
+              <StyledTableCell component="th" scope="row">
+                {index+1}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.firstname}</StyledTableCell>
+              <StyledTableCell align="right">{row.lastname}</StyledTableCell>
+              <StyledTableCell align="right">{row.age}</StyledTableCell>
+              <StyledTableCell align="right">{row.email}</StyledTableCell>
+              <StyledTableCell align="right">{row.state}</StyledTableCell>
+              <StyledTableCell align="right">{row.city}</StyledTableCell>
+              <StyledTableCell align="right">
               {<Chip 
                     label="remove"
                     onDelete={()=>{
@@ -135,35 +163,29 @@ const  onDelete = (index)=> {
                     color='error'
                     deleteIcon={<RemoveCircleOutlineOutlinedIcon/>}
                     /> }
-              </TableCell>
+              </StyledTableCell>
               
               
 
-            </TableRow>) : null
+            </StyledTableRow>) : null
            
           ))}
           
         </TableBody>
-        
+  
           </Table>
-
+         
         </TableContainer>
+        
         <Button variant="contained" onClick={handleAddRow}>
           Add Row
         </Button>
 
         <Dialog open={showForm} onClose={handleCloseForm}>
           <DialogTitle>Add New Row</DialogTitle>
-          <DialogContent>
-            <TextField
-              name="id"
-              value={newRowData.id}
-              onChange={handleInputChange}
-              label="Id"
-              fullWidth
-              margin="dense"
-            />
-            <TextField
+          <DialogContent>        
+            
+              <TextField
               name="firstname"
               value={newRowData.firstname}
               onChange={handleInputChange}
@@ -171,6 +193,7 @@ const  onDelete = (index)=> {
               fullWidth
               margin="dense"
             />
+            
             <TextField
               name="lastname"
               value={newRowData.lastname}
@@ -179,6 +202,7 @@ const  onDelete = (index)=> {
               fullWidth
               margin="dense"
             />
+            
             <TextField
               name="age"
               value={newRowData.age}
@@ -187,6 +211,7 @@ const  onDelete = (index)=> {
               fullWidth
               margin="dense"
             />
+            
             <TextField
               name="email"
               value={newRowData.email}
@@ -195,6 +220,7 @@ const  onDelete = (index)=> {
               fullWidth
               margin="dense"
             />
+            
             <TextField
               name="state"
               value={newRowData.state}
@@ -203,6 +229,7 @@ const  onDelete = (index)=> {
               fullWidth
               margin="dense"
             />
+            
             <TextField
               name="city"
               value={newRowData.city}
@@ -211,15 +238,17 @@ const  onDelete = (index)=> {
               fullWidth
               margin="dense"
             />
-          </DialogContent>
+            
           <DialogActions>
             <Button onClick={handleCloseForm}>Cancel</Button>
             <Button onClick={handleAddNewRow}>Add</Button>
           </DialogActions>
+          </DialogContent>
         </Dialog>
       </Container> }
 
    </div>
+   
   );
 }
 
